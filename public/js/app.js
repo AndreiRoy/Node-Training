1a35440cc95f423f442c80fe09fe9c0deddefdf2
Roy <royffire@gmail.com>
2019-08-20 06:17:27 -0600

    Remove unnecessary spaces

--- public/js/app.js
+++ public/js/app.js
@@ -17,12 +17,10 @@ console.log('The javascript is up and running')
 
     })
 }) */
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const message1 = document.querySelector('#message-1')
 const message2 = document.querySelector('#message-2')
-
 
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
